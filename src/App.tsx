645060695c848d43751c7acfed086d6068b82b88
Kuldik <112172212+Kuldik@users.noreply.github.com>
2024-03-16 13:38:39 +0000

    create porudct

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {useState} from 'react'
 import { Product } from './components/Product';
+import {products} from './data/products';
 
 function App() {
  
  
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      <Product/>
+      <Product product={products[0]}  />
+      <Product product={products[1]}  />
     </div>
   )
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,32 @@
+import { IProduct } from "../models"
+import { useState } from "react"
 
-export const Product = () => {
+interface ProductProps {
+  product: IProduct
+}
+
+export const Product = ({product}: ProductProps) => {
+  const [details, setDetails] = useState(false)
+
   return (
-    <div>Product</div>
+    <div 
+      className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+    >
+      <img src={product.image} alt={product.title} className="w-2/6" />
+      <p className="text-2xl text-center">{ product.title }</p>
+      <span className="text-red-600 font-bold">Price:</span> <p className="font-bold underline">{ product.price }</p>
+      <span className="text-red-600 font-bold">Rating:</span> <span className="font-bold underline">{ product.rating.rate }</span>
+      <button 
+        className="border py-2 px-4 rounded text-red-600 bg-gradient-to-r from-blue-300 to-azure-400 hover:from-green-600 hover:to-blue-400 transition-all duration-300 ease-in hover:text-purple-900 hover:border-yellow-500 hover:bg-yellow-400 outline-none rounded-ss-[40px] rounded-se-[0px] rounded-ee-[40px] rounded-es-[0px] hover:rounded-es-[40px] hover:rounded-se-[40px] hover:rounded-ss-[0px] ease"
+        onClick={() => setDetails(prev => !prev)} 
+        // Switching between true and false
+      >
+        {details ? "Hide Details" : "Show Details"}
+      </button>
+      {details && <div>
+        <p>{ product.description } </p>
+      </div>}
+      
+    </div>
   )
 }
